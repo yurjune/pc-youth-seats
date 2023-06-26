fc9aa1b67bc115e75a60b288db38d6c00123671a
yurjune <yurjune@naver.com>
2023-06-27 02:12:00 +0900

    feat: migrate CRA to VITE

--- front/src/shared/constants/index.ts
+++ front/src/shared/constants/index.ts
@@ -1,13 +1,9 @@
-const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
-const SERVER_URL = IS_DEVELOPMENT ? 'http://localhost:5000' : 'http://3.34.59.214:5000';
-const GA_ID = process.env.REACT_APP_GA_ID;
-const ADMIN_PW = process.env.REACT_APP_ADMIN_PW ?? '';
-const REDEEMUS_PW = process.env.REACT_APP_REDEEMUS_PW ?? '';
+const SERVER_URL = import.meta.env.DEV ? 'http://localhost:5000' : 'http://3.34.59.214:5000';
+const GA_ID = import.meta.env.VITE_APP_GA_ID;
+const ADMIN_PW = import.meta.env.VITE_APP_ADMIN_PW ?? '';
+const REDEEMUS_PW = import.meta.env.VITE_APP_REDEEMUS_PW ?? '';
 
 interface Env {
-  IS_PRODUCTION: boolean;
-  IS_DEVELOPMENT: boolean;
   SERVER_URL: string;
   GA_ID: string | undefined;
   ADMIN_PW: string;
@@ -15,8 +11,6 @@ interface Env {
 }
 
 export const env: Env = {
-  IS_PRODUCTION,
-  IS_DEVELOPMENT,
   SERVER_URL,
   GA_ID,
   ADMIN_PW,
