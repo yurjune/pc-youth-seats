647d965cf1ed39819ab64d61888450d8bbf41b5b
Honey <yurjune@naver.com>
2023-07-03 16:12:27 +0900

    refactor: scheduling 로직 분리

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,23 +1,13 @@
 import express from 'express';
-import fs from 'fs';
 import path from 'path';
 import bodyParser from 'body-parser';
-import schedule from 'node-schedule';
 import helmet from 'helmet';
 import cors from 'cors';
 import history from 'connect-history-api-fallback';
-import launchSocketIO from './socket';
 
-import { getYearMonthDate, getKoreanTime } from './utils';
+import launchSocketIO from './socket';
+import enrollSchedules from './scheduler';
 import { cancelReservation, getLastWeekSeats, getSeats, makeReservation } from './controllers';
-import {
-  CURRENT_SEATS,
-  FULL_SEATS,
-  LAST_WEEK_SEATS,
-  BACKUP_DIRECTORY,
-  HISTORY_DIRECTORY,
-  JSON_DIRECTORY,
-} from './constants';
 
 global.lateSeatIds = [];
 global.absentSeatIds = [];
@@ -31,40 +21,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(helmet());
 app.set('port', process.env.PORT || 5000);
 
-// 초 분 시간 일 월 요일
-// 월요일 자정에 초기화
-schedule.scheduleJob('00 00 00 * * 1', () => {
-  fs.readFile(`${JSON_DIRECTORY}/${FULL_SEATS}`, 'utf8', (err, result) => {
-    if (err) return console.log('시온채플 좌석 리셋 실패');
-
-    fs.writeFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, result, () => {
-      lateSeatIds = [];
-      absentSeatIds = [];
-      console.log('시온채플 좌석 리셋 완료');
-    });
-  });
-
-  fs.readFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
-    fs.writeFile(`${JSON_DIRECTORY}/${LAST_WEEK_SEATS}`, result, () => {
-      console.log('지난주 좌석 저장 완료');
-    });
-
-    fs.writeFile(`${HISTORY_DIRECTORY}/seats_${getYearMonthDate()}.json`, result, (err) => {
-      if (err) return console.log(`좌석 히스토리 저장 실패`);
-      console.log(`좌석 히스토리 저장 완료`);
-    });
-  });
-});
-
-schedule.scheduleJob('00 00 * * * *', () => {
-  fs.readFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
-    const { hour } = getKoreanTime();
-
-    fs.writeFile(`${BACKUP_DIRECTORY}/seats_backup_${hour}.json`, result, (err) => {
-      if (err) return console.log(`좌석 백업 실패`);
-    });
-  });
-});
+enrollSchedules();
 
 export const expressServer = app.listen(app.get('port'), () => {
   console.log('WebServer Port: ' + app.get('port'));
---
+++ back/src/scheduler.ts
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import schedule from 'node-schedule';
+
+import { getYearMonthDate, getKoreanTime } from './utils';
+import {
+  CURRENT_SEATS,
+  FULL_SEATS,
+  LAST_WEEK_SEATS,
+  BACKUP_DIRECTORY,
+  HISTORY_DIRECTORY,
+  JSON_DIRECTORY,
+} from './constants';
+
+const resetAllSeats = () => {
+  fs.readFile(`${JSON_DIRECTORY}/${FULL_SEATS}`, 'utf8', (err, result) => {
+    if (err) return console.log('시온채플 좌석 리셋 실패');
+
+    fs.writeFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, result, () => {
+      global.lateSeatIds = [];
+      global.absentSeatIds = [];
+      console.log('시온채플 좌석 리셋 완료');
+    });
+  });
+};
+
+const saveLastWeekSeats = () => {
+  fs.readFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
+    fs.writeFile(`${JSON_DIRECTORY}/${LAST_WEEK_SEATS}`, result, () => {
+      console.log('지난주 좌석 저장 완료');
+    });
+
+    fs.writeFile(`${HISTORY_DIRECTORY}/seats_${getYearMonthDate()}.json`, result, (err) => {
+      if (err) return console.log(`좌석 히스토리 저장 실패`);
+      console.log(`좌석 히스토리 저장 완료`);
+    });
+  });
+};
+
+const backupSeats = () => {
+  fs.readFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
+    const { hour } = getKoreanTime();
+
+    fs.writeFile(`${BACKUP_DIRECTORY}/seats_backup_${hour}.json`, result, (err) => {
+      if (err) return console.log(`좌석 백업 실패`);
+    });
+  });
+};
+
+const enrollSchedules = () => {
+  // 초 분 시간 일 월 요일
+  const MONDAY_MIDNIGHT = '00 00 00 * * 1';
+  const EVERY_HOUR = '00 00 * * * *';
+
+  schedule.scheduleJob(MONDAY_MIDNIGHT, () => {
+    resetAllSeats();
+    saveLastWeekSeats();
+  });
+
+  schedule.scheduleJob(EVERY_HOUR, backupSeats);
+};
+
+export default enrollSchedules;
