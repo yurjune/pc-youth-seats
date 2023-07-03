42de22e5c01f96072574879e55326ee575e4b4c8
Honey <yurjune@naver.com>
2023-07-03 14:31:18 +0900

    refactor: controller 분리

--- back/src/app.ts
+++ back/src/app.ts
@@ -8,39 +8,21 @@ import cors from 'cors';
 import history from 'connect-history-api-fallback';
 import { Server } from 'socket.io';
 
+import { checkIsLateReservation, getYearMonthDate, getKoreanTime } from './utils';
+import type { ClientToServerEvents, ServerToClientEvents } from './models';
+import { cancelReservation, getLastWeekSeats, getSeats, makeReservation } from './controllers';
 import {
-  checkIsAvailableForReservation,
-  checkIsLateReservation,
-  getYearMonthDate,
-  getKoreanTime,
-  logWithTime,
-} from './utils';
-import type {
-  CancelReservationReq,
-  CancelReservationRes,
-  ClientToServerEvents,
-  GetLastWeekSeatsRes,
-  GetSeatsRes,
-  MakeReservationReq,
-  MakeReservationRes,
-  Seats,
-  ServerToClientEvents,
-  TypedReq,
-  TypedRes,
-} from './models';
+  CURRENT_SEATS,
+  FULL_SEATS,
+  LAST_WEEK_SEATS,
+  BACKUP_DIRECTORY,
+  HISTORY_DIRECTORY,
+  JSON_DIRECTORY,
+} from './constants';
 
 const app = express();
-const fsPromise = fs.promises;
 let lateSeatIds: string[] = [];
 let absentSeatIds: string[] = [];
-
-// paths
-const jsonDirectory = path.join(__dirname, '../json');
-const historyDirectory = path.join(__dirname, '../../../seats_history');
-const backupDirectory = path.join(__dirname, '../../../seats_backup');
-const FULL_SEATS = 'seats(full).json';
-const CURRENT_SEATS = 'seats.json';
-const LAST_WEEK_SEATS = 'seats_last_week.json';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -53,22 +35,22 @@ app.set('port', process.env.PORT || 5000);
 // 초 분 시간 일 월 요일
 // 월요일 자정에 초기화
 schedule.scheduleJob('00 00 00 * * 1', () => {
-  fs.readFile(`${jsonDirectory}/${FULL_SEATS}`, 'utf8', (err, result) => {
+  fs.readFile(`${JSON_DIRECTORY}/${FULL_SEATS}`, 'utf8', (err, result) => {
     if (err) return console.log('시온채플 좌석 리셋 실패');
 
-    fs.writeFile(`${jsonDirectory}/${CURRENT_SEATS}`, result, () => {
+    fs.writeFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, result, () => {
       lateSeatIds = [];
       absentSeatIds = [];
       console.log('시온채플 좌석 리셋 완료');
     });
   });
 
-  fs.readFile(`${jsonDirectory}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
-    fs.writeFile(`${jsonDirectory}/${LAST_WEEK_SEATS}`, result, () => {
+  fs.readFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
+    fs.writeFile(`${JSON_DIRECTORY}/${LAST_WEEK_SEATS}`, result, () => {
       console.log('지난주 좌석 저장 완료');
     });
 
-    fs.writeFile(`${historyDirectory}/seats_${getYearMonthDate()}.json`, result, (err) => {
+    fs.writeFile(`${HISTORY_DIRECTORY}/seats_${getYearMonthDate()}.json`, result, (err) => {
       if (err) return console.log(`좌석 히스토리 저장 실패`);
       console.log(`좌석 히스토리 저장 완료`);
     });
@@ -76,10 +58,10 @@ schedule.scheduleJob('00 00 00 * * 1', () => {
 });
 
 schedule.scheduleJob('00 00 * * * *', () => {
-  fs.readFile(`${jsonDirectory}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
+  fs.readFile(`${JSON_DIRECTORY}/${CURRENT_SEATS}`, 'utf8', (err, result) => {
     const { hour } = getKoreanTime();
 
-    fs.writeFile(`${backupDirectory}/seats_backup_${hour}.json`, result, (err) => {
+    fs.writeFile(`${BACKUP_DIRECTORY}/seats_backup_${hour}.json`, result, (err) => {
       if (err) return console.log(`좌석 백업 실패`);
     });
   });
@@ -143,109 +125,7 @@ app.all('/*', (req, res, next) => {
   next();
 });
 
-app.get('/api/getSeats', (_, res: TypedRes<GetSeatsRes>) => {
-  const jsonFile = fs.readFileSync(`${jsonDirectory}/${CURRENT_SEATS}`, 'utf8');
-  const parsedJSON = JSON.parse(jsonFile);
-  return res.send(parsedJSON);
-});
-
-app.get('/api/getLastWeekSeats', (_, res: TypedRes<GetLastWeekSeatsRes>) => {
-  const jsonFile = fs.readFileSync(`${jsonDirectory}/${LAST_WEEK_SEATS}`, 'utf8');
-  const parsedJSON = JSON.parse(jsonFile);
-  return res.send(parsedJSON);
-});
-
-app.put(
-  '/api/makeReservation',
-  (req: TypedReq<MakeReservationReq>, res: TypedRes<MakeReservationRes>) => {
-    if (!checkIsAvailableForReservation()) {
-      return res.send({ ok: false, message: '예약 가능한 시간대가 아닙니다.' });
-    }
-
-    const { params } = req.body;
-    fs.readFile(`${jsonDirectory}/${CURRENT_SEATS}`, 'utf8', (err, data) => {
-      const parsedJSON: Seats = JSON.parse(data);
-
-      for (const idx in parsedJSON[params.line]) {
-        if (parsedJSON[params.line][idx].id !== params.id) {
-          continue;
-        }
-
-        if (parsedJSON[params.line][idx].seat_active === 5) {
-          return res.send({ ok: false, message: '이미 예약된 좌석입니다.' });
-        }
-
-        parsedJSON[params.line][idx].seat_active = params.seat_active;
-        parsedJSON[params.line][idx].name = params.name;
-        parsedJSON[params.line][idx].pw = params.pw;
-        break;
-      }
-
-      fs.writeFile(`${jsonDirectory}/${CURRENT_SEATS}`, JSON.stringify(parsedJSON), (err) => {
-        if (err) {
-          return res.send({ ok: false, message: 'Something went wrong.' });
-        }
-
-        logWithTime(`예약완료: ${params.id}, ${params.name}, ${params.pw}`);
-        return res.send({ ok: true, message: '예약 되었습니다.' });
-      });
-    });
-  },
-);
-
-app.put(
-  '/api/cancelReservation',
-  (req: TypedReq<CancelReservationReq>, res: TypedRes<CancelReservationRes>) => {
-    const { params } = req.body;
-    fsPromise
-      .readFile(`${jsonDirectory}/${FULL_SEATS}`, 'utf8')
-      .then((file) => {
-        const parsedJSON: Seats = JSON.parse(file);
-        let defaultSeatActive = 0;
-        let defaultSeatName = '';
-
-        for (const seat of parsedJSON[params.line]) {
-          if (seat.id === params.id) {
-            defaultSeatActive = seat.seat_active;
-            defaultSeatName = seat.name;
-            break;
-          }
-        }
-
-        return { defaultSeatActive, defaultSeatName };
-      })
-      .then(({ defaultSeatActive, defaultSeatName }) => {
-        fs.readFile(`${jsonDirectory}/${CURRENT_SEATS}`, 'utf8', (err, data) => {
-          const parsedJSON: Seats = JSON.parse(data);
-
-          for (const idx in parsedJSON[params.line]) {
-            if (parsedJSON[params.line][idx].id === params.id) {
-              parsedJSON[params.line][idx].seat_active = defaultSeatActive;
-              parsedJSON[params.line][idx].name = defaultSeatName;
-              parsedJSON[params.line][idx].pw = '';
-              break;
-            }
-          }
-
-          fs.writeFile(`${jsonDirectory}/${CURRENT_SEATS}`, JSON.stringify(parsedJSON), (err) => {
-            if (err) {
-              return res.send({
-                ok: false,
-                message: 'Something went wrong',
-                defaultSeatActive,
-                defaultSeatName,
-              });
-            }
-
-            logWithTime(`삭제완료: ${params.id}`);
-            return res.send({
-              ok: true,
-              message: '삭제 되었습니다.',
-              defaultSeatActive,
-              defaultSeatName,
-            });
-          });
-        });
-      });
-  },
-);
+app.get('/api/getSeats', getSeats);
+app.get('/api/getLastWeekSeats', getLastWeekSeats);
+app.put('/api/makeReservation', makeReservation);
+app.put('/api/cancelReservation', cancelReservation);
---
+++ back/src/constants.ts
@@ -0,0 +1,9 @@
+import path from 'path';
+
+// paths
+export const JSON_DIRECTORY = path.join(__dirname, '../json');
+export const HISTORY_DIRECTORY = path.join(__dirname, '../../../seats_history');
+export const BACKUP_DIRECTORY = path.join(__dirname, '../../../seats_backup');
+export const FULL_SEATS = 'seats(full).json';
+export const CURRENT_SEATS = 'seats.json';
+export const LAST_WEEK_SEATS = 'seats_last_week.json';
