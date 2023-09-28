5739ae8576ab7464a9dc2c4e59e1df7f13c6a1ff
yurjune <yurjune@naver.com>
2023-09-28 22:33:09 +0900

    fix: express.static 미들웨어의 위치를 cors, connect-history-api-fallback 미들웨어 아래로 변경: root(/) 페이지 이외 페이지에 다이렉트 접속 시 html 파일을 받지 못하는 문제를 해결

--- back/src/app.ts
+++ back/src/app.ts
@@ -14,9 +14,9 @@ global.absentSeatIds = [];
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
 app.use(history());
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(helmet());
 app.set('port', 5100);
 
