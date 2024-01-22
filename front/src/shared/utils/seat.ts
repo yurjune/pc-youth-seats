5568d024beecbf5e7b290de907ec205cfca05164
yurjune <yurjune@naver.com>
2024-01-23 00:11:08 +0900

    feat: 사역팀 지정석 대응

--- front/src/shared/utils/seat.ts
+++ front/src/shared/utils/seat.ts
@@ -1,6 +1,6 @@
 import { Seats } from '@shared/models/seat.model';
 
-export const APPOINTED_SEATS = ['교역자', '방송팀', '새가족', '05또래'];
+export const APPOINTED_SEATS = ['교역자', '방송팀', '새가족', '05또래', '사역팀'];
 export const ACTIVE_SEATS = [1, 4, 5];
 
 export const getNumberOfSeats = (seats: Seats) => {
