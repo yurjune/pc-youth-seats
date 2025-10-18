b61f0639c4de4680d89583fd9790b7e9468d8158
yurjune <yurjune@naver.com>
2025-10-18 21:39:58 +0900

    refactor: migrate seat_active

--- back/src/models/seat.model.ts
+++ back/src/models/seat.model.ts
@@ -13,10 +13,10 @@ export interface SeatParams extends Pick<Seat, 'id' | 'seat_active' | 'name' | '
 }
 
 export enum SeatActive {
-  PATH = 0, // 복도
-  VACANT = 1, // 예약 가능
-  DISABLED = 2, // 예약 불가
-  PRIVATE = 4, // 지정석
-  RESERVED = 5, // 예약됨
-  INVISIBLE = 6, // 공간만 차지하는 투명 좌석
+  VACANT = 0, // 예약 가능
+  DISABLED = 1, // 예약 불가
+  PRIVATE = 2, // 지정석
+  RESERVED = 3, // 예약됨
+  INVISIBLE = 4, // 공간만 차지하는 투명 좌석
+  PATH = 5, // 복도
 }
